6d2a50bfd00c77556c85df270daaeb2d8a81a5cc
vaarunx <varunrishwandh2001@gmail.com>
2025-01-14 19:45:55 -0500

    Initial commit

---
+++ src/Kambaz/Account/index.tsx
@@ -0,0 +1,31 @@
+import { Routes } from "react-router-dom";
+import Signin from "./Signin";
+import { Navigate, Route } from "react-router";
+import Profile from "./Profile";
+import Signup from "./Signup";
+import AccountNavigation from "./Navigation";
+
+export default function Account() {
+  return (
+    <div id="wd-account-screen">
+      <table>
+        <tr>
+          <td valign="top">
+            <AccountNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route
+                path="/"
+                element={<Navigate to="/Kambaz/Account/Signin" />}
+              />
+              <Route path="/Signin" element={<Signin />} />
+              <Route path="/Profile" element={<Profile />} />
+              <Route path="/Signup" element={<Signup />} />
+            </Routes>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
