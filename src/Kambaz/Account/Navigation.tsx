e171d5a615078bd34af39be91ed35d7a52d41cdc
vaarunx <varunrishwandh2001@gmail.com>
2025-02-04 20:16:18 -0500

    fixed active pane in account navigation

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -3,8 +3,8 @@ export default function AccountNavigation() {
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
       <Link to={`/Kambaz/Account/Signin`} className="list-group-item active border border-0"> Signin </Link>
-      <Link to={`/Kambaz/Account/Signup`} className="list-group-item active border border-0"> Signup </Link> 
-      <Link to={`/Kambaz/Account/Profile`} className="list-group-item active border border-0"> Profile </Link>
+      <Link to={`/Kambaz/Account/Signup`} className="list-group-item text-danger border border-0"> Signup </Link> 
+      <Link to={`/Kambaz/Account/Profile`} className="list-group-item text-danger border border-0"> Profile </Link>
     </div>
   );
 }
