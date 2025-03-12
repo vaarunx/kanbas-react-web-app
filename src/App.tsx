b492fe8037a83e1064c69e523a1f229519d3c421
vaarunx <varunrishwandh2001@gmail.com>
2025-03-12 13:26:53 -0400

    Completed Labs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Labs from "./Labs";
+// import store from "./Kambaz/store";
+// import { Provider } from "react-redux";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Kambaz from "./Kambaz";
 export default function App() {
   return (
     <HashRouter>
+       {/* <Provider store={store}> */}
       <div>
         <Routes>
           <Route path="/" element={<Navigate to="Kambaz" />} />
@@ -11,6 +14,8 @@ export default function App() {
           <Route path="/Kambaz/*" element={<Kambaz />} />
         </Routes>
       </div>
+      {/* </Provider> */}
     </HashRouter>
+
   );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,8 +3,12 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
+import { Provider } from "react-redux";
+import store from "./store";
 export default function Labs() {
   return (
+    <Provider store = {store}>
     <div>
       <h1> CS5610 - 01 Spring 2025</h1>
       <h1> Varun Rishwandh Sekar</h1>
@@ -15,6 +19,9 @@ export default function Labs() {
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2/*" element={<Lab2 />} />
         <Route path="Lab3/*" element={<Lab3 />} />
+        <Route path="Lab4/*" element={<Lab4 />} />
       </Routes>
     </div>
+    </Provider>
 );}
+
