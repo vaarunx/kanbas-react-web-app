07ac324a31ba219fefb8a56fcec6ff779ce14b64
vaarunx <varunrishwandh2001@gmail.com>
2025-03-12 22:27:51 -0400

    Fixed navigation overlay issue

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -12,33 +12,17 @@ export default function Account() {
 
   return (
     <div id="wd-account-screen">
-      <table>
-        <tr>
-          <td valign="top">
-            <AccountNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <Navigate
-                    to={
-                      currentUser
-                        ? "/Kambaz/Account/Profile"
-                        : "/Kambaz/Account/Signin"
-                    }
-                  />
-                }
-              />
-
-              <Route path="/Signin" element={<Signin />} />
-              <Route path="/Profile" element={<Profile />} />
-              <Route path="/Signup" element={<Signup />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+      <div className="d-none d-md-block">
+        <AccountNavigation />
+      </div>
+      <div className="wd-main-content-offset" style={{maxWidth: "500px", width: "100%"}}>
+        <Routes>
+        <Route path="/" element={<Navigate to={ currentUser ? "/Kambaz/Account/Profile" : "/Kambaz/Account/Signin" }/>}/>
+        <Route path="/Signin" element={<Signin />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Signup" element={<Signup />} />
+        </Routes>
+      </div>
     </div>
   );
 }
